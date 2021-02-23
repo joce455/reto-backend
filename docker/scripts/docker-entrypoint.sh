75a31e8bd99dd01465eed6f8f6ff3563fa505800
EXB76264 <jose.juarez1@claro.com.ar>
2021-02-22 23:14:38 -0300

    firts commit

--- docker/scripts/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-touch ${TOMCAT_BASE}/logs/access_log.log && chown -R ${USERNAME}:${GROUPNAME} ${TOMCAT_BASE} && tail -F ${TOMCAT_BASE}/logs/access_log.log &
-
-
-cd ${TOMCAT_BASE}
-exec su-exec ${USERNAME}:${GROUPNAME} java -jar $JAVA_OPTS ${TOMCAT_BASE}/${SPRINGBOOT_APP_JAR}  --server.port=$SERVER_PORT
